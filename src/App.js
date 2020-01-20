f46e632235c9a19d3384cde9940dbda5fabc20e9
Ramprasad-R <ramprasad.info@gmail.com>
2020-01-20 23:31:51 +0100

    Added header and footer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 class App extends Component {
   render() {
-    return <Header />;
+    return (
+      <div>
+        <Header />
+        <Footer />;
+      </div>
+    );
   }
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,12 @@
 import React, { Component } from "react";
-import { Navbar } from "react-bootstrap";
 import logo from "./spoonoh_logo.png";
-import "./Header.css";
+import "./style.css";
 export default class Header extends Component {
   render() {
     return (
-      <>
-        <Navbar bg="primary" variant="dark">
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src={logo}
-              width="80"
-              height="80"
-              className="d-inline-block align-top"
-            />{" "}
-            <span className="center-navbar">Spoon Oh!</span>
-          </Navbar.Brand>
-        </Navbar>
-      </>
+      <header>
+        <img src={logo} alt="logo" />
+      </header>
     );
   }
 }
