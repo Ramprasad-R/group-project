a45146cb292ed1f2f21154fd47f6846197b1564d
Ramprasad-R <ramprasad.info@gmail.com>
2020-01-20 23:47:51 +0100

    checking API KEY in netlify

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+console.log("Check API Key", process.env.REACT_APP_API_KEY);
+
 ReactDOM.render(<App />, document.getElementById("root"));
