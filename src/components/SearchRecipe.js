224230c2bd19bebb7b9064ed8ed999c9e404aa72
jendhordejan <jendhordejan@gmail.com>
2020-01-22 16:57:18 +0100

    added search form component

--- src/components/SearchRecipe.js
+++ src/components/SearchRecipe.js
@@ -1,17 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Tab } from "react-bootstrap";
 
 export default class SearchRecipe extends Component {
-    render() {
-        return (
-            <div className="search-recipe">
-                <form>
-                    <label>
-                        Search Recipe for:
-                <input type="text" name="txtSearch" />
-                    </label>
-                    <input type="submit" value="Search" />
-                </form>
-            </div>
-        )
-    }
+  state = { name: "" };
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(`Submitting form with query`, this.state.name);
+  };
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="search-recipe">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Search Recipe for:
+            <input
+              type="text"
+              name="name"
+              onChange={this.handleChange}
+              //   value={this.state.query}
+            />
+          </label>
+          <input type="submit" value="Search" />
+        </form>
+      </div>
+    );
+  }
 }
