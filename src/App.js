e57cecab1220ba4607b4549ebdac5fc06a1c9698
Ramprasad-R <ramprasad.info@gmail.com>
2020-01-23 16:27:36 +0100

    Added the Link and page route to product ingredients page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import FetchData from "./components/FetchData";
+import { Route } from "react-router-dom";
+import ProductIngredients from "./components/ProductIngredients";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <FetchData />
+        <Route exact path="/" component={FetchData} />
+        <Route
+          exact
+          path="/productingredients/:productId"
+          component={ProductIngredients}
+        />
         <Footer />
       </div>
     );
--- src/components/RecipeCards.jsx
+++ src/components/RecipeCards.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./RecipeCards.css";
+import { Link } from "react-router-dom";
 export default class RecipeCard extends React.Component {
   render() {
     const { recipeID, imageUrl, title } = this.props;
     return (
       <div className="recipeCard" id={recipeID}>
+        <Link to={`/productingredients/${recipeID}`}>
+          {" "}
+          <img src={imageUrl} alt={title}></img>
+          <p>
+            <em>{title}</em>
+          </p>
+        </Link>
         <img src={imageUrl} alt={title}></img>
         <p>
           <em>{title}</em>
