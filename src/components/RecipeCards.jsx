eb9345143cedbc2198a64f8cfb13117973476f62
jendhordejan <jendhordejan@gmail.com>
2020-01-24 15:41:11 +0100

    added like function

--- src/components/RecipeCards.jsx
+++ src/components/RecipeCards.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import "./RecipeCards.css";
 import { Link } from "react-router-dom";
 export default class RecipeCard extends React.Component {
+  state = {
+    likeCounter: 0
+  };
+  increaseLikes = () => {
+    this.setState({ likeCounter: this.state.likeCounter + 1 });
+  };
   render() {
     const { recipeID, imageUrl, title } = this.props;
     return (
@@ -12,6 +18,16 @@ export default class RecipeCard extends React.Component {
             <em>{title}</em>
           </p>
         </Link>
+        <div>
+          <text>{this.state.likeCounter}</text>
+          <div>
+            <i
+              class="fas fa-thumbs-up"
+              id={recipeID}
+              onClick={this.increaseLikes}
+            ></i>
+          </div>
+        </div>
       </div>
     );
   }
